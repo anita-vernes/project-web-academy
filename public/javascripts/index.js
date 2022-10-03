908d1db535322b48b216d860333357dce2f79304
anita-vernes <anita.vernes@softvision.com>
2022-10-03 18:21:37 +0300

    new

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,12 @@
 import { memberCards} from "./memberContainer.js";
-import { formChecker } from "./validation.js";
+import { validation } from "./validation.js";
 import {backBtn} from "./backBtn.js"
 import { addMembers } from "./addMembers.js";
 import { updateMembers } from "./updateMembers.js";
 
 export const baseUrl = "http://localhost:3000/users";
 memberCards();
-formChecker();
 backBtn();
 addMembers();
 updateMembers();
+validation();
